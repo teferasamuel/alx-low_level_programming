f11e89016eef1f9d49276ea0057873d12201f5b8
teferasamuel <teferasamuel67@gmail.com>
2022-06-02 09:28:38 -0700

    Putting in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- * return: Always 0 (Success) 
- */
-
+ * Return: Always 0 (Success)
+*/
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multiningual puzzle");
-	return(0);
+	return (0);
 }
