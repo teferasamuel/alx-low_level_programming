0022d226b06d9d9089ac2b64a47af8f6db0b0e68
teferasamuel <teferasamuel67@gmail.com>
2022-06-02 09:09:38 -0700

    e

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main- Entry point
- * return: Always 0 
+ * main - Entry point
+ * return: Always 0 (Success) 
  */
 
 int main(void)
 { 
-	puts( "\"Programming is like building a multiningual puzzle\n");
+	puts("\"Programming is like building a multiningual puzzle");
 	return(0);
 }
