43e8fa46fe71a1ad26941a8bc59609393cd081fa
teferasamuel <teferasamuel67@gmail.com>
2022-06-02 11:17:01 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * return: ends the function
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of a char : %lu byte(s)\n", sizeof(a));
+	printf("size of an int : %lu byte(s)\n", sizeof(b));
+	printf("size of a long int : %lu byte(s)\n", sizeof(c));
+	printf("size of a long long int : %lu byte(s)\n", sizeof(d));
+	printf("size of a float : %lu byte(s)\n", sizeof(e));
+	return (0);
+}
