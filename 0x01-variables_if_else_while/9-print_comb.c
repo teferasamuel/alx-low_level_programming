a610beb940d2eb9533a0f6726a5696927b44f192
teferasamuel <teferasamuel67@gmail.com>
2022-06-03 10:42:43 -0700

    Ordering numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - possible combination of numbers
+ * return: ends the function
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
