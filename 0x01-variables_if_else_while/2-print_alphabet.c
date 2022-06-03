94231f789fdacb88263e29ef62850cdbec1bd8db
teferasamuel <teferasamuel67@gmail.com>
2022-06-03 09:01:07 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char A;
+
+	for (A = 'a' ; A <= 'z' ; A++)
+		putchar(A);
+	putchar('\n');
+	return (0);
+}
