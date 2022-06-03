8570991791495b3b3112c0e3811b32be6fb6bd64
teferasamuel <teferasamuel67@gmail.com>
2022-06-03 09:49:14 -0700

    Number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: end of function
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+		printf("%i", num);
+	putchar('\n');
+	return (0);
+}
