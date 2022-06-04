14b06b8e01c1778cda085dec510cbb432521e236
teferasamuel <teferasamuel67@gmail.com>
2022-06-03 22:48:47 -0700

    Three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,17 +9,23 @@ int main(void)
 	int tens;
 	int ones;
 
-	for (hundreds = 0 ; hundreds <= 9 ; hundreds++)
+	for (hundreds = 0 ; hundreds <= 7  ; hundreds++)
 	{
-		for (tens = hundreds + 1 ; tens <= 9 ; tens++)
+		for (tens = hundreds + 1 ; tens <= 8 ; tens++)
 		{
 			for (ones = tens + 1 ; ones <= 9 ; ones++)
 			{
-				putchar(hundreds + '0');
-				putchar(tens + '0');
-				putchar(ones + '0');
-				if (hundreds <= 7 && tens <= 8)
+				if (hundreds == 7 && tens == 8 && ones == 9)
 				{
+					putchar(hundreds + '0');
+					putchar(tens + '0');
+					putchar(ones + '0');
+				}
+				else
+				{
+					putchar(hundreds + '0');
+					putchar(tens + '0');
+					putchar(ones + '0');
 					putchar(',');
 					putchar(' ');
 				}
