00cf34ac14adc5a2f5cb1ef9821f6788e06cc5f3
teferasamuel <teferasamuel67@gmail.com>
2022-06-03 23:54:00 -0700

    Print numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return: ends the function
+ *Return: Always 0
  */
 int main(void)
 {
 	int num;
 
-	for (num = '0' ; num <= '9' ; num++)
+	for (num = 0 ; num <= 9 ; num++)
 	{
 		putchar(num + '0');
 		if (num < 9)
