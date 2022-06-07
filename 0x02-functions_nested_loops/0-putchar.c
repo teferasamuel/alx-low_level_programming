83de632beeb4abad7141315b462a001819e56af3
teferasamuel <teferasamuel67@gmail.com>
2022-06-07 11:10:00 -0700

    Main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main()
+{
+	_putchar();
+	return 0;
+}
+void _putchar()
+{
+	_putchar("_putchar");
+	_putchar('\n');
+}
+
