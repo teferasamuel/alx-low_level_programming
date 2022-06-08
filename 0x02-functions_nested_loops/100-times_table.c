9be8278227b3e0add08876972346f14a8ef92159
teferasamuel <teferasamuel67@gmail.com>
2022-06-08 05:52:03 -0700

    Print time table for nubers between 0 &15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 				else
 				{
 					_putchar(((op / 100) % 10) + 48);
-					_puthar(((op / 10) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
 					_putchar((op % 10) + 48);
 				}
 			}
