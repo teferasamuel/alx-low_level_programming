71842b40200b973ca5a66c3dbbbec4e6bd27deb4
teferasamuel <teferasamuel67@gmail.com>
2022-06-13 01:03:00 -0700

    Update to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 - function that takes apointer to an int
- * @n: integer 
- * return: void
+ *reset_to_98 - function that takes apointer to an int
+ *@n: integer
+ *return: void
  */
 void reset_to_98(int *n)
 {
