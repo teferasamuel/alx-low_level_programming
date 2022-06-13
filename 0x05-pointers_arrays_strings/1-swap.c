525d050f2ed4d3a7ddfc96c3bbfad98a7fd6572b
teferasamuel <teferasamuel67@gmail.com>
2022-06-13 01:36:44 -0700

    Swap two numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * swap_int - a function to swap the value of two integers
+ * swap_int - a function to swap the value 
+ * of two integers
  * @a: integer value of num 1
  * @b: integer value of num 2
  * @c: integer
