590352a30bff7d68ba4d65fd499499cfa5173ae0
teferasamuel <teferasamuel67@gmail.com>
2022-06-20 02:10:49 -0700

    Copy memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*function that copies memory area
+*_memcpy -, function that copies memory area
 *@dest: memory where is stored
 *@src: memory where is copied
 *@n: number of bytes
