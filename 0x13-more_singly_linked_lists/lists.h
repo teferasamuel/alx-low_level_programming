5e5b59603913905989ef47da167c090035185d4a
teferasamuel <teferasamuel67@gmail.com>
2022-07-19 11:08:02 -0700

    It works

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -29,4 +29,5 @@ typedef struct listp_s
 } listp_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
